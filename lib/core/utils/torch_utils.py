2f095112eac4a9c33df5edd33b747f7ccea2fd55
liangzi <liangzi@staff.sina.com.cn>
2021-01-14 10:39:30 +0800

    add seed

--- lib/core/utils/torch_utils.py
+++ lib/core/utils/torch_utils.py
@@ -36,3 +36,20 @@ class EMA():
                 assert name in self.backup
                 param.data = self.backup[name]
         self.backup = {}
+
+
+
+
+import numpy as  np
+import random
+import torch
+
+
+def seed_everything(seed):
+    random.seed(seed)
+    np.random.seed(seed)
+    torch.manual_seed(seed)  # cpu
+    torch.cuda.manual_seed_all(seed)
+    torch.backends.cudnn.deterministic = True
+    torch.backends.cudnn.benchmark = True
+
--- train_config.py
+++ train_config.py
@@ -6,11 +6,18 @@ from configs.mscoco.mbv2_config import config as mb2_config
 from configs.mscoco.shufflenet_5x5_config import config as shufflenet_config
 from configs.mscoco.resnet18_config import config as resnet_config
 ##### the config for different task
-config=resnet_config
+config=mb2_config
 
 os.environ["CUDA_VISIBLE_DEVICES"] = "0"
 config.TRAIN.num_gpu = 1
 
+
+
+
+
+seed=42
+from lib.core.utils.torch_utils import seed_everything
+seed_everything(seed)
 
 
 
